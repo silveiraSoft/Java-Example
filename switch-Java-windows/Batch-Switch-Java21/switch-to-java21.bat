7524a7c0986fd1cbdacdfc37d51c1a784f443ead
asilveir <asilveir@proteccion.com.co>
2025-06-04 08:38:20 -0400

    Switching Java Version

---
+++ switch-Java-windows/Batch-Switch-Java21/switch-to-java21.bat
@@ -0,0 +1,4 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Amazon Corretto\jdk21.0.7_6
+set PATH=%JAVA_HOME%\bin;%PATH%
+java -version
---
+++ switch-Java-windows/Batch-Switch-Java8/switch-to-java8.bat
@@ -0,0 +1,4 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Semeru\jdk-8.0.392.8-openj9
+set PATH=%JAVA_HOME%\bin;%PATH%
+java -version
