c12945fe147e009bd289b1ec23cdb4e852b73ed6
silveiraSoft <adalbertosn1982@gmail.com>
2025-06-04 13:26:03 -0400

    fist coming

---
+++ SecondLessFrequency/SecondLessFrequency.java
@@ -0,0 +1,6 @@
+public class SecondLessFrequency {
+    public static void main(String[] args) {
+        // This is a simple Java program that prints a message to the console
+        System.out.println("This is my second less frequent message.");
+    } 
+}
